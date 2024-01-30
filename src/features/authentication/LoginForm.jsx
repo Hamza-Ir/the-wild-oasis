de4d5cb399d28e88dde1eea46a7c1f9812504d22
Hamza Irfan <Hamza1AtWork@gmail.com>
2024-01-30 18:41:57 +0500

    final commits

--- src/features/authentication/LoginForm.jsx
+++ src/features/authentication/LoginForm.jsx
@@ -7,8 +7,8 @@ import { useLogin } from "./useLogin.js";
 import SpinnerMini from "../../ui/SpinnerMini.jsx";
 
 function LoginForm() {
-  const [email, setEmail] = useState("hamza@example.com");
-  const [password, setPassword] = useState("pass12345");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const { login, isLoading } = useLogin();
 
   function handleSubmit(e) {
