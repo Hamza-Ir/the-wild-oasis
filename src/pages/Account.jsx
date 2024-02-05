4287ebbbcd2138a14c631a84437408033b9b52ba
Hamza Irfan <Hamza1AtWork@gmail.com>
2024-02-05 17:45:09 +0500

    testing git

--- src/pages/Account.jsx
+++ src/pages/Account.jsx
@@ -6,13 +6,12 @@ import Row from "../ui/Row";
 function Account() {
   return (
     <>
+      //testing git
       <Heading as="h1">Update your account</Heading>
-
       <Row>
         <Heading as="h3">Update user data</Heading>
         <UpdateUserDataForm />
       </Row>
-
       <Row>
         <Heading as="h3">Update password</Heading>
         <UpdatePasswordForm />
