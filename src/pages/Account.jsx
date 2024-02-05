ee48650a5be32a76de6adb8664a2a3e64a81cf01
Hamza Irfan <Hamza1AtWork@gmail.com>
2024-02-05 17:46:09 +0500

    final commit v3

--- src/pages/Account.jsx
+++ src/pages/Account.jsx
@@ -6,7 +6,6 @@ import Row from "../ui/Row";
 function Account() {
   return (
     <>
-      //testing git
       <Heading as="h1">Update your account</Heading>
       <Row>
         <Heading as="h3">Update user data</Heading>
